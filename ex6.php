dbdf4adb81d960f3be799aabb589836d7466da3d
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-05 20:09:36 +0200

    ex.6 php

---
+++ ex6.php
@@ -0,0 +1,21 @@
+<?php
+ 
+
+ function ay_consonant($str){
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $consonants = '';
+    $i = 0;
+    while ($i < strlen($str) && !in_array($str[$i], $vowels)){
+        $consonants .= $str[$i];
+        $i++;
+    }
+    if ($i == 0) {
+        $result = $str . 'ay';
+    } else {
+        $result = substr($str, $i) . $consonants . 'ay';
+    }
+    return $result;
+}
+    
+  echo json_encode(ay_consonant($_GET['str']))
+?>
