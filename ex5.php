ef1f14583880615a954bbe3ad47d90ed72ca1641
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-05 22:09:51 +0200

    ex. prime and even or not

--- ex5.php
+++ ex5.php
@@ -1,1 +1,35 @@
- 
+<?php
+
+$year = $_POST['year'];
+$age = 2023 - $year;
+function isPrime($age)
+{
+ for($i=2; $i<$age; $i++)
+   {
+      if($age %$i ==0)
+	      {
+		   return 0;
+		  }
+    }
+  return 1;
+   }
+   function isEven($n){
+    if($n % 2 ==0 )
+        {
+            return 0;
+        }
+        return 1;
+   }
+$prime = isPrime($age);
+$even = isEven($age);
+if ($prime == 1 && $even == 0){
+  $response["Age"]="prime and even" ;
+  echo json_encode($response);
+}
+
+else{
+  $response["Age"]="not prime or not even" ;
+  echo json_encode($response);
+}
+
+?>
