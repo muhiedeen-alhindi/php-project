180d8ccefbb9505b8027a0591ea42fe24bbd7c66
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-05 15:15:08 +0200

    validate form useing php

---
+++ php.php
@@ -0,0 +1,38 @@
+ 
+ <?php
+ 
+
+$email = $password=  "";
+ 
+ 
+
+  if (empty($_POST["email"])) {
+     $response["status"]=" email is required " ;
+  } else if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    $response["status"]='Invalid email format.';
+  }
+
+  else {$email = ($_POST["email"]); 
+
+  if (empty($_POST["password"])) {
+      $response["status"]=" password is required ";
+  } else if (strlen($_POST["password"]) < 8){
+    $response["status"]=" password should be >8 ";
+  }
+  else if (!preg_match('/[A-Z]/', $_POST["password"])) {
+   $response["status"]="Password should contain at least one uppercase letter";}
+  
+
+  else {$password = ($_POST["password"]);
+    $response["status"]="success" ; }
+
+
+  }
+ 
+  
+ 
+ 
+ 
+echo json_encode($response);
+
+?>
